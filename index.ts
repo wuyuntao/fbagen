1ab219da7e464f46e83b97d25a2dc422d5c34f6f
Wu Yuntao <wyt.brandon@gmail.com>
2016-10-17 18:26:52 +0800

    1, Create VS solution

---
+++ index.ts
@@ -0,0 +1,30 @@
+﻿const meow = require("meow");
+
+const cli = meow(`
+  Usage
+    $ fbagen <files>
+
+  Options
+    -o, --output-path       Output generated files to path
+    -n, --csharp            Generate C# code
+
+  Examples
+    $ fbagen -o GeneratedCode -n Schema.fbs
+`, {
+        alias: {
+            o: 'output-path',
+            n: 'csharp',
+        },
+        string: ["output-path"],
+        boolean: ["csharp"],
+    });
+
+if (cli.input.length > 0) {
+    console.log("files");
+    console.log(cli.input);
+    console.log("flags");
+    console.log(cli.flags);
+}
+else {
+    console.info(cli.help);
+}
